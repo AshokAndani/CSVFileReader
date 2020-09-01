30c5126bb9d351046b7ab8ec4756eb49d4f76600
ashok34589 <ashok34589@gmail.com>
2020-09-01 10:41:58 +0530

    Add CSVReader

---
+++ CSVReader/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace CSVReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ErrorTextBox = new System.Windows.Forms.Label();
+            this.DropDownBox = new System.Windows.Forms.DomainUpDown();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1335, 529);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(185, 561);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(436, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 564);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "FileName :";
+            // 
+            // ErrorTextBox
+            // 
+            this.ErrorTextBox.AutoSize = true;
+            this.ErrorTextBox.ForeColor = System.Drawing.Color.Red;
+            this.ErrorTextBox.Location = new System.Drawing.Point(119, 597);
+            this.ErrorTextBox.Name = "ErrorTextBox";
+            this.ErrorTextBox.Size = new System.Drawing.Size(0, 13);
+            this.ErrorTextBox.TabIndex = 3;
+            // 
+            // DropDownBox
+            // 
+            this.DropDownBox.Location = new System.Drawing.Point(739, 557);
+            this.DropDownBox.Name = "DropDownBox";
+            this.DropDownBox.Size = new System.Drawing.Size(120, 20);
+            this.DropDownBox.TabIndex = 4;
+            this.DropDownBox.Text = "FilterBy";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnLoad.Location = new System.Drawing.Point(917, 559);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(118, 23);
+            this.btnLoad.TabIndex = 5;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnBrowse.Location = new System.Drawing.Point(616, 558);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 6;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Red;
+            this.btnClose.Location = new System.Drawing.Point(1101, 559);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1359, 619);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.DropDownBox);
+            this.Controls.Add(this.ErrorTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "CSV File Reader";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ErrorTextBox;
+        private System.Windows.Forms.DomainUpDown DropDownBox;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ CSVReader/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSVReader
+{
+    public partial class Form1 : Form
+    {
+        private string[] ColumnLabels;
+        private List<string[]> Data;
+        private HashSet<string> DropDownData;
+        private DataTable Table;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DropDownBox.Enabled = false;
+            DropDownData = new HashSet<string>();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.ShowDialog();
+            textBox1.Text = fileDialog.FileName;
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            Table = new DataTable();
+            Data = new List<string[]>();
+            if (DropDownBox.Enabled == false)
+            {
+                DropDownBox.Enabled = true;
+            }
+            var fileData = File.ReadAllLines(textBox1.Text);
+            ColumnLabels = fileData[0].TrimEnd(',').Split(',');
+            foreach (string data in ColumnLabels)
+            {
+                Table.Columns.Add(new DataColumn(data));
+            }
+
+            //// Filling the Data into the List
+            for (int i = 1; i < fileData.Length; i++)
+            {
+                var rowData = fileData[i].TrimEnd(',').Split(',');
+                string[] tempArray = new string[rowData.Length];
+                for (int j = 0; j < rowData.Length; j++)
+                {
+                    tempArray[j] = rowData[j];
+                    if (j == 1)
+                    {
+                        DropDownData.Add(rowData[j]);
+                    }
+                }
+                Data.Add(tempArray);
+            }
+            FillGrid(Data, DropDownBox.Text);
+        }
+        private void FillGrid(List<string[]> data, string Filter)
+        {
+            List<string[]> tempList;
+            if (Filter == "FilterBy")
+            {
+                tempList = Data;
+            }
+            else
+            {
+                tempList = Data.Where(q => q[1]==Filter).ToList();
+                
+            }
+
+            foreach (string[] rowArray in tempList)
+            {
+                DataRow dataRow = Table.NewRow();
+                for (int i = 0; i < rowArray.Length; i++)
+                {
+                    dataRow[ColumnLabels[i]] = rowArray[i];
+                }
+                Table.Rows.Add(dataRow);
+            }
+            dataGridView1.DataSource = Table;
+            int rowNum = 0;
+            foreach(string[] rowArray in tempList)
+            {
+                var last = double.Parse(rowArray[6]);
+                var prev = double.Parse(rowArray[7]);
+                if (last>prev)
+                {
+                    dataGridView1.Rows[rowNum++].Cells[0].Style.BackColor = Color.Green;
+                    ////dataGridView1.Rows[rowNum++].DefaultCellStyle.BackColor = Color.Green;
+                }
+                else if(last < prev)
+                {
+                    dataGridView1.Rows[rowNum++].Cells[0].Style.BackColor = Color.Red;
+                    ////dataGridView1.Rows[rowNum++].DefaultCellStyle.BackColor = Color.Red;
+                }
+                else if(last == prev)
+                {
+                    dataGridView1.Rows[rowNum++].Cells[0].Style.BackColor = Color.Yellow;
+                    ////dataGridView1.Rows[rowNum++].DefaultCellStyle.BackColor = Color.Yellow;
+                }
+            }
+            foreach(var someData in DropDownData)
+            {
+                DropDownBox.Items.Add(someData);
+            }
+        }
+    }
+}
